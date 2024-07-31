d7275cd13e50c3b10a6c715d6bbd7e05990243f4
Ethan <etgold05@gmail.com>
2024-07-31 17:36:35 +0300

    added stuff

---
+++ main.cpp
@@ -0,0 +1,34 @@
+
+#include <string>
+using namespace std;
+int main(int argc, char *argv[]) {
+    return 0;
+}
+
+void nothing() {
+    return;
+}
+
+void open_file(string filename) {
+    
+}
+
+void add_numbers(int a, int b) {
+
+}
+
+void time() {
+
+}
+
+void ping() {
+
+}
+
+void write_file(string filename, string content) {
+
+}
+
+void run_ls(string dirname) {
+
+}
