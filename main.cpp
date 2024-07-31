09279d8698fbdcc212975a8ad6537dd8b115de7a
Ethan <etgold05@gmail.com>
2024-07-31 17:52:26 +0300

    added openfile

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 
 #include <string>
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
 using namespace std;
+
 int main(int argc, char *argv[]) {
     return 0;
+
 }
 
 void nothing() {
-    return;
+    usleep(990000);
 }
 
 void open_file(string filename) {
-    
+    cerr << "trying to open file " << filename << endl;
+    usleep(900000);
+    int fd = open(filename.c_str(), O_RDWR);
+    char* buf = new char[1];
+    buf[0] = 'a';
+    write(fd, buf, 1);
+    cerr << "opened file " << filename << endl;
 }
 
 void add_numbers(int a, int b) {
@@ -19,6 +30,7 @@ void add_numbers(int a, int b) {
 
 void time() {
 
+
 }
 
 void ping() {
@@ -26,7 +38,6 @@ void ping() {
 }
 
 void write_file(string filename, string content) {
-
 }
 
 void run_ls(string dirname) {
